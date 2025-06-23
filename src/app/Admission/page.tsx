c1366199dee341429c5ce5ece3d70305cbe6ac80
hatimchharchhoda <hitparker123@gmail.com>
2025-06-23 15:00:33 +0530

    solving error while deploy

--- src/app/Admission/page.tsx
+++ src/app/Admission/page.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import PatientAdmissionForm from "@/components/PatientAdmissionForm";
 
-const AdmissionPage = () => {
-  return <PatientAdmissionForm />;
-};
-
-export default AdmissionPage;
+export default function AdmissionPage() {
+  return (
+    <Suspense fallback={<div>Loading form...</div>}>
+      <PatientAdmissionForm />
+    </Suspense>
+  );
+}
